73ddcb5142a1e8e8b1906db3f44afd20a49859e1
Peter Engelbert <pmengelbert@gmail.com>
2020-01-08 12:00:29 -0600

    fixed debug

--- client.go
+++ client.go
@@ -43,6 +43,7 @@ func NewClient(address string, opts ...clientOption) (*Client, error) {
 	client := Client{}
 	client.Client = resty.New()
 	client.Config = conf
+	client.Debug = conf.Debug
 
 	// For client transport, use reg's multilayer RoundTripper for "Docker-style" auth
 	client.SetTransport(&reg.BasicTransport{
