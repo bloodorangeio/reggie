b5da1d5bf756ac803a6dee9a6218455312e7b2cd
Peter Engelbert <pmengelbert@gmail.com>
2020-01-14 11:55:54 -0600

    modified auth code to handle basic

--- auth.go
+++ auth.go
@@ -2,9 +2,15 @@ package reggie
 
 import (
 	"encoding/json"
+	"errors"
 	"regexp"
+	"strings"
 
 	"github.com/mitchellh/mapstructure"
+)
+
+var (
+	authHeaderMatcher = regexp.MustCompile("(?i).*(bearer|basic).*")
 )
 
 type (
@@ -25,31 +31,40 @@ func (client *Client) retryRequestWithAuth(originalRequest *Request, originalRes
 	if authHeaderRaw == "" {
 		return originalResponse, nil
 	}
-	h := parseAuthHeader(authHeaderRaw)
-	req := client.Client.NewRequest().
-		SetQueryParam("service", h.Service).
-		SetQueryParam("scope", h.Scope).
-		SetHeader("Accept", "application/json").
-		SetHeader("User-Agent", client.Config.UserAgent).
-		SetBasicAuth(client.Config.Username, client.Config.Password)
-	authResp, err := req.Execute(GET, h.Realm)
-	if err != nil {
-		return nil, err
+
+	authenticationType := authHeaderMatcher.ReplaceAllString(authHeaderRaw, "$1")
+	if strings.EqualFold(authenticationType, "bearer") {
+		h := parseAuthHeader(authHeaderRaw)
+		req := client.Client.NewRequest().
+			SetQueryParam("service", h.Service).
+			SetQueryParam("scope", h.Scope).
+			SetHeader("Accept", "application/json").
+			SetHeader("User-Agent", client.Config.UserAgent).
+			SetBasicAuth(client.Config.Username, client.Config.Password)
+		authResp, err := req.Execute(GET, h.Realm)
+		if err != nil {
+			return nil, err
+		}
+
+		var info authInfo
+		bodyBytes := authResp.Body()
+		err = json.Unmarshal(bodyBytes, &info)
+		if err != nil {
+			return nil, err
+		}
+
+		token := info.Token
+		if token == "" {
+			token = info.AccessToken
+		}
+		originalRequest.SetAuthToken(token)
+		return originalRequest.Execute(originalRequest.Method, originalRequest.URL)
+	} else if strings.EqualFold(authenticationType, "basic") {
+		originalRequest.SetBasicAuth(client.Config.Username, client.Config.Password)
+		return originalRequest.Execute(originalRequest.Method, originalRequest.URL)
 	}
 
-	var info authInfo
-	bodyBytes := authResp.Body()
-	err = json.Unmarshal(bodyBytes, &info)
-	if err != nil {
-		return nil, err
-	}
-
-	token := info.Token
-	if token == "" {
-		token = info.AccessToken
-	}
-	originalRequest.SetAuthToken(token)
-	return originalRequest.Execute(originalRequest.Method, originalRequest.URL)
+	return nil, errors.New("Something went wrong with authorization")
 }
 
 func parseAuthHeader(authHeaderRaw string) *authHeader {
